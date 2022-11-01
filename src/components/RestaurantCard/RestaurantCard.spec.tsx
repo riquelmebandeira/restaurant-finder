b527beb58380b830daed2f30361944a0bac5b16e
riquelme <riquelmebandeira1@outlook.com>
2022-11-01 10:10:25 -0300

    add tests to RestaurantCard component

---
+++ src/components/RestaurantCard/RestaurantCard.spec.tsx
@@ -0,0 +1,30 @@
+import { screen } from '@testing-library/react'
+import RestaurantCard from '.'
+import { renderWithRouterAndTheme } from '../../styles/render-theme'
+
+const props = {
+  id: 1,
+  name: 'arabek',
+  image: ''
+}
+
+describe('<RestaurantCard />', () => {
+  it('should render correctly', () => {
+    renderWithRouterAndTheme(<RestaurantCard {...props} />)
+
+    const img = screen.getByRole('img', { name: /foto do restaurante arabek/i })
+    const link = screen.getByRole('link', { name: /arabek/i })
+
+    expect(img).toBeInTheDocument()
+    expect(link).toBeInTheDocument()
+    expect(link).toHaveAttribute('href', `/restaurant/${props.id}`)
+  })
+
+  it('should match the snapshot', () => {
+    const { container } = renderWithRouterAndTheme(
+      <RestaurantCard {...props} />
+    )
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
--- src/styles/render-theme.js
+++ src/styles/render-theme.js
@@ -1,7 +1,16 @@
-import { render } from '@testing-library/react';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './theme';
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './theme'
 
 export const renderTheme = (children) => {
-  return render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
-};
+  return render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
+}
+
+export const renderWithRouterAndTheme = (children) => {
+  return render(
+    <ThemeProvider theme={theme}>
+      <MemoryRouter>{children}</MemoryRouter>
+    </ThemeProvider>
+  )
+}
