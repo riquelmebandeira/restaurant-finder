22c6e685d062222b24eefc9a8e3a4348b7e17dcc
riquelme <riquelmebandeira1@outlook.com>
2022-11-03 13:54:46 -0300

    add default level value to styled heading

--- src/components/Typography/Heading.ts
+++ src/components/Typography/Heading.ts
@@ -8,7 +8,7 @@ type HeadingProps = {
   level?: 1 | 2 | 3 | 4 | 5 | 6
 }
 
-const Heading = styled.h1.attrs<HeadingProps>(({ level }) => ({
+const Heading = styled.h1.attrs<HeadingProps>(({ level = 1 }) => ({
   as: `h${level}`
 }))<HeadingProps>`
   ${({ color = 'white', size = 'huge', fontWeight = 800 }) => css`
